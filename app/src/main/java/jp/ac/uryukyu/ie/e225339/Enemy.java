f74fc2add006fb4dad78d65eeef5f6d17183698d
NAKATA AREI <arei@NAKATAnoMacBook-Pro.local>
2023-11-28 06:16:12 +0900

    step1

--- app/src/main/java/jp/ac/uryukyu/ie/e225339/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e225339/Enemy.java
@@ -8,11 +8,8 @@ package jp.ac.uryukyu.ie.e225339;
  *  boolean dead; //敵の生死状態。true=死亡。
  * Created by tnal on 2016/11/13.
  */
-public class Enemy {
-    public String name;
-    public int hitPoint;
-    public int attack;
-    public boolean dead;
+public class Enemy extends LivingThing{
+    
 
     /**
      * コンストラクタ。名前、最大HP、攻撃力を指定する。
@@ -21,27 +18,7 @@ public class Enemy {
      * @param attack モンスターの攻撃力
      */
     public Enemy (String name, int maximumHP, int attack) {
-        this.name = name;
-        hitPoint = maximumHP;
-        this.attack = attack;
-        dead = false;
-        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
-    }
-
-    /**
-     * Heroへ攻撃するメソッド。
-     * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
-     * @param hero 攻撃対象
-     */
-    public void attack(Hero hero){
-        int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.name, damage);
-        hero.wounded(damage);
-        if(this.dead == false){
-            int damege = (int)(Math.random() * attack);
-            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.name, damage);
-            hero.wounded(damage);
-        }
+        super(name, maximumHP, attack);
     }
 
     /**
