8681dfb7d1a4d2f2e97024e6f8e04787edbc68c7
NAKATA AREI <arei@NAKATAnoMacBook-Pro.local>
2023-11-28 05:44:04 +0900

    first commit

---
+++ app/src/test/java/jp/ac/uryukyu/ie/e225339/App.java
@@ -0,0 +1,11 @@
+package jp.ac.uryukyu.ie.e225339;
+
+public class App {
+    public String getGreeting(){
+        return "Hello World!";
+    }
+
+    public static void main(String[] args){
+        System.out.println(new App().getGreeting());
+    }
+}
